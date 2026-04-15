908cc8d27034592a9219b9be28c1ed29ad2d7a4b
taskin8651 <mdsayebalam10@gmail.com>
2026-04-15 11:24:01 +0530

    dd

--- public/deploy.php
+++ public/deploy.php
@@ -5,7 +5,7 @@ $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'] ?? '';
 $secret = 'mysecret'; // optional
 
 // Go to your project folder
-$projectRoot = '/home/smartuni/public_html/estimate.sanketkumarofficial.com';
+$projectRoot = '/home/smartuni/public_html/msasuite.web-link.site';
 
 chdir($projectRoot);
 
